5b5683b418651ba56d17e963681216c62fe49580
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-29 10:27:29 -0600

    Improved Routing for Weather Module.

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -9,12 +9,12 @@ import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
-  declarations: [WeatherViewComponent, WeatherItemComponent, WeatherSearchComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    MaterialModule,
-    WeatherRoutingModule
-  ]
+  declarations: [
+    WeatherViewComponent,
+    WeatherItemComponent,
+    WeatherSearchComponent
+  ],
+  imports: [CommonModule, SharedModule, MaterialModule, WeatherRoutingModule],
+  exports: [WeatherViewComponent, WeatherItemComponent, WeatherSearchComponent]
 })
-export class WeatherModule { }
+export class WeatherModule {}
