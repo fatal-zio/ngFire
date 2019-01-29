3901644b94b59e6cb1426378230e7ab98b05b673
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-29 09:44:55 -0600

    Further Module setup.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material.module';
+import { ToolbarComponent } from '../ui/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedModule,
-    MaterialModule
-  ]
+  declarations: [ToolbarComponent],
+  imports: [CommonModule, SharedModule, MaterialModule],
+  exports: [ToolbarComponent]
 })
-export class CoreModule { }
+export class CoreModule {}
