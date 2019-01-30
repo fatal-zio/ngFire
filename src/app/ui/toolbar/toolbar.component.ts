fbb0c3d819f47f277ee6c66713102da838537f7d
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-30 17:07:32 -0600

    Improved Nav Menu. Added some basic logic around Login/Logout.

--- src/app/ui/toolbar/toolbar.component.ts
+++ src/app/ui/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from 'src/app/user/auth.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,12 +10,12 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public authService: AuthService
+  ) {}
 }
--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../shared/interfaces/user';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: Observable<User | null>;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    public afAuth: AngularFireAuth,
+    private snackBar: MatSnackBar
+  ) {
+    this.user = this.afAuth.authState;
+  }
+
+  public logout(): void {
+    this.afAuth.auth.signOut().then(() => {
+      this.snackBar.open('Logged out successfully.', 'OK', {
+        duration: 5000
+      });
+      this.router.navigate(['/weather']);
+    });
+  }
 }
