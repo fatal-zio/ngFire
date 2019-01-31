1e577e7d6beabab45618bd84bf1763c5e7e5863e
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-31 10:24:04 -0600

    Expanded Authentication code.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthProvider } from 'ngx-auth-firebaseui';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public providers = AuthProvider;
 
-  constructor() { }
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private afAuth: AngularFireAuth
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public success(content: any): void {
+    this.snackBar.open(`Welcome ${content.displayName}`, 'OK', {
+      duration: 5000
+    });
+    this.router.navigate(['/weather']);
   }
-
 }
--- src/app/weather/weather-search/weather-search.component.ts
+++ src/app/weather/weather-search/weather-search.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
 import { Weather } from 'src/app/shared/interfaces/weather';
 import { WeatherDataService } from '../weather-data.service';
-import { AuthProvider } from 'ngx-auth-firebaseui';
 
 @Component({
   selector: 'app-weather-search',
@@ -11,7 +10,6 @@ import { AuthProvider } from 'ngx-auth-firebaseui';
 })
 export class WeatherSearchComponent implements OnInit {
   public query: string;
-  public providers = AuthProvider;
 
   constructor(
     private weatherService: WeatherService,
